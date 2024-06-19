1e8ba82136504563ea0b6f938427c08edbf92d1c
yaelscholten <yaelscholten1@gmail.com>
2024-06-19 11:55:31 +0200

    user razor deel gemaakt

---
+++ DemoCovadis.Shared/Requests/UserRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoCovadis.Shared.Requests;
+
+public class UserRequest
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Wachtwoord { get; set; }
+
+    [Required]
+    public string Naam { get; set; }
+}
