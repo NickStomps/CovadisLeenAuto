4053d9e57a079963d633bfd2aaffaadb88162bed
yaelscholten <yaelscholten1@gmail.com>
2024-06-20 13:12:14 +0200

    user aanmaken

--- DemoCovadis.Shared/Clients/UserHttpClient.cs
+++ DemoCovadis.Shared/Clients/UserHttpClient.cs
@@ -1,4 +1,6 @@
 ﻿using DemoCovadis.Shared.Dtos;
+using DemoCovadis.Shared.Requests;
+using System.Text;
 using System.Text.Json;
 
 namespace DemoCovadis.Shared.Clients;
@@ -38,4 +40,39 @@ public class UserHttpClient
 
         return users;
     }
+
+    public async Task<UserDto?> CreateUser(UserRequest request)
+    {
+        var jsonContent = JsonSerializer.Serialize(request);
+        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(string.Empty, content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var createdUser = JsonSerializer.Deserialize<UserDto>(responseContent, jsonOptions);
+
+        return createdUser;
+    }
+
+    public async Task<UserDto?> UpdateUser(int id, UserRequest request)
+    {
+        var jsonContent = JsonSerializer.Serialize(request);
+        var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+        var response = await client.PutAsync($"/{id}", content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var updatedUser = JsonSerializer.Deserialize<UserDto>(responseContent, jsonOptions);
+
+        return updatedUser;
+    }
+
 }
